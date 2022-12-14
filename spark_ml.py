f3a6ed64f66f0e39072110f8298da89d60d2d7cc
Juliana Braga <juliana4braga@gmail.com>
2022-12-13 21:45:27 -0300

    Linear Regression and Decision Tree Regression

--- spark_ml.py
+++
@@ -1,9 +0,0 @@
-from pyspark.sql import SparkSession
-from pyspark.sql import functions as f
-
-spark = SparkSession \
-        .builder \
-        .appName("Regressão com Spark") \
-        .getOrCreate()
-
-
